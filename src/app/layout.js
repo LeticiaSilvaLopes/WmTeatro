5d76a988fc84224431e69c1862fb1f1d5ced6f7d
leticiaLopes <83143656+LeticiaSilvaLopes@users.noreply.github.com>
2025-10-14 18:40:58 -0300

    Adicionando o link da política de privacidade no footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
         </main>
         <footer className="bg-black border-t border-gray-800 text-white py-8 text-center">
           <p>&copy; 2025 <span className="text-purple-400">WM Teatro</span>. Todos os direitos reservados.</p>
+          <p href='/politica-de-privacidade'>Política de Privacidade</p>
         </footer>
       </body>
     </html>
